123aeab9900459e7dfd3a4e150c597d1301e9064
eslamber <eslamber@student.42lyon.fr>
2024-06-22 10:26:50 +0200

    save des fichiers

--- srcs/requirements/mariadb/tools/conf.sh
+++ srcs/requirements/mariadb/tools/conf.sh
@@ -7,22 +7,22 @@ set -e
 service mariadb start;
 
 # Creation d'une table
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DATABASE}\`;"
 
 # Creation d'un utilisateur qui puisse manipuler cette table
-mysql -e "CREATE USER IF NOT EXITS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXITS \`${DATA_USER}\`@'localhost' IDENTIFIED BY '${PASS}';"
 
 # Don de tous les droits a cette utilisateur pour la table creee
-mysql -e "GRANT ALL PRIVILIGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILIGES ON \`${DATABASE}\`.* TO \`${DATA_USER}\`@'%' IDENTIFIED BY '${PASS}';"
 
 # Modification des droits du root
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
 
 # Refresh de mysql
 mysql -e "FLUSH PRIVILEGES"
 
 # Redemarrage de mysql
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p$ROOT_PASS shutdown
 
 # Execution de mysql avec le mode safe
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -14,18 +14,18 @@ if ! wp core is-installed --path=${INSTAL_WP}; then
 	wp core download --path=${INSTAL_WP} --allow-root
 	# Creation de la config
 	wp config creat \
-		--dbname ${DB_NAME}\
-		--dbuser ${DB_USER}\
-		--dbhost ${DB_HOST}\
-		--dbpass ${DB_PASS}\
+		--dbname ${DATABASE} \
+		--dbuser ${DATA_USER} \
+		--dbhost ${HOST} \
+		--dbpass ${PASS} \
 		--path=${INSTAL_WP} --allow-root
 	# Creation du site et de l'admin
 	wp core install \
-		--url= ${URL}\
-		--title= ${TITLE}\
-		--admin_user= ${ADMIN}\
-		--admin_password= ${ADMIN_PASS}\
-		--admin_email= ${ADMIN_MAIL}\
+		--url= ${URL} \
+		--title= ${TITLE} \
+		--admin_user= ${ADMIN} \
+		--admin_password= ${ADMIN_PASS} \
+		--admin_email= ${ADMIN_MAIL} \
 		--path=${INSTAL_WP} --allow-root
 	# Creation du User
 	wp user creat ${USER} ${USER_MAIL} --role=${USER_ROLE} --user_pass=${USER_PASS} --path=${INSTAL_WP} --allow-root
